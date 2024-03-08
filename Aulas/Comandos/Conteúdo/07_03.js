2985616bffc1459bd8ce93571a11200248c9d5f4
JoaoMagalhaes-hub <jgabrielomm@gmail.com>
2024-03-07 21:15:39 -0300

    Uso de Array

---
+++ Aulas/Comandos/Conteúdo/07_03.js
@@ -0,0 +1,5 @@
+var alunos = ["Pedro da Silva", "Maria da Silva", "João"];
+alunos[0]  // Array é tipo uma list
+
+alunos.push[Glaucio] // Push irá adicionar o elemento no último espaço do array 
+alunos.unshift[Glaucio] // Unshift faz o contrário e irá colocar no início
