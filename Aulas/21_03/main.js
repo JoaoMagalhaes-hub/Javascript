ba9e88d3d8643b155c71e4c64745624075036554
JoaoMagalhaes-hub <jgabrielomm@gmail.com>
2024-03-21 20:07:55 -0300

    Objetos

---
+++ Aulas/21_03/main.js
@@ -0,0 +1,14 @@
+var aluno = {
+    nome: "João da Silva", 
+    idade: 26,
+    nacionalidade: "Brasileiro",
+    menorIdade: false,
+    filiacao: ["Joao Silva Pai, Maria silva Mae"],
+    
+    endereco: {
+        rua: "Rua tal",
+        bairro: "Bairro tal",
+        cidade:" João Pessoa"},
+    
+    estaRegular: function() {return true;}
+}
